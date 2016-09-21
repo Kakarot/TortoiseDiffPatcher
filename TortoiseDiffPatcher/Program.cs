4a84824051491d2482c6f501fbb8de92e0884cc9
Kakarot <JamesRuiz@gatech.edu>
2016-09-21 12:40:03 -0400

    creation of VS solution and default project.

---
+++ TortoiseDiffPatcher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TortoiseDiffPatcher
+{
+    /*
+     * One Day this program will do something.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
